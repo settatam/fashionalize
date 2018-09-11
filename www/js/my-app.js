09db21a544d164fffb57ec5b95a1144de782c78d
Seth Atam <sethatam@Seths-MacBook-Pro.local>
2018-09-11 19:49:02 -0400

    Push image changes

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -479,11 +479,9 @@ var win = function (r) {
   alert(user_images.length)
   images_length = user_images.length;
   var all_images = $$(document).find('.slide-photo');
-  image_string = '<div class"slide-photo"><img src="'+ active_image +'" /></div>';
+  image_string = '<div class="slide-photo"><img src="'+ active_image +'" /></div>';
   $$('.slide-container').html(image_string);
   if(user_images.length == 4) {
-    $$('#image-response').html('<p> You have uploaded as many images as needed </p>')
-  }else if(user_images.length == 1) {
     $$('#image-response').html('<p> You have uploaded as many images as needed </p>')
   }else{
     $$('#image-response').html('<p> '+ image_responses[user_images.length-1] +' </p>')
